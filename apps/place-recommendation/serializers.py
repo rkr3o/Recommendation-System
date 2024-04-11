680c23ce2ac150c12b68b74455404f2503f77f2b
= <=>
2024-04-11 11:29:27 +0530

    add

--- apps/place-recommendation/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Activity
-
-
-class ActivitySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Activity
-        fields = ["id", "name", "description", "place"]
